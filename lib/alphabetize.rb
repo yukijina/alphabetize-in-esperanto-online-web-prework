fa459558670f7f8f3febff725b43fe3b4c1244b9
Yuki N <lunarbase@outlook.com>
2018-11-18 21:12:01 +0000

    automatically backed up by learn

--- lib/alphabetize.rb
+++ lib/alphabetize.rb
@@ -1,6 +1,6 @@
 def alphabetize(arr)
   # code here
   alphabet = "abcĉdefgĝhĥijĵklmnoprsŝtuŭvz"
-  sorted_arr = arr.sort_by {|a| alphabet.index(a)}
-  sorted_arr.sort_by
+  #sorted_arr = arr.sort_by {|a| alphabet.index(a)}
+  arr.sort_by
 end
