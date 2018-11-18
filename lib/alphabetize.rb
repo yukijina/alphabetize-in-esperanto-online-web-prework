c55149494813498494cbf5831eb3bbd703007dae
Yuki N <lunarbase@outlook.com>
2018-11-18 21:21:54 +0000

    automatically backed up by learn

--- lib/alphabetize.rb
+++ lib/alphabetize.rb
@@ -1,7 +1,6 @@
 def alphabetize(arr)
   esperanto = "abcĉdefgĝhĥijĵklmnoprsŝtuŭvz"
   ascii = "@ABCDEFGHIJKLMNOPQRSTUVWXYZ[\\"
-  #replace esperanto order to ascii order
-  #sorted = arr.sort
+  #replace esperanto order to ascii order 
   arr.sort_by {|a| a.tr(esperanto, ascii)}
 end
