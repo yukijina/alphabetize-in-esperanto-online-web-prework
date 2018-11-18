2d770e1035f58d53a5ea5af08e73221b188a8051
Yuki N <lunarbase@outlook.com>
2018-11-18 21:17:30 +0000

    automatically backed up by learn

--- lib/alphabetize.rb
+++ lib/alphabetize.rb
@@ -1,6 +1,7 @@
 def alphabetize(arr)
   # code here
   alphabet = "abcĉdefgĝhĥijĵklmnoprsŝtuŭvz"
+  ascii = "@ABCDEFGHIJKLMNOPQRSTUVWXYZ[\\"
   sorted = arr.sort
   sorted.sort_by {|a| alphabet.index(a)}
 end
